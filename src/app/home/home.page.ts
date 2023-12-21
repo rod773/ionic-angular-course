0366ae09c1d05440d33a39c73ec0339fe7e90035
rod773 <104315743+rod773@users.noreply.github.com>
2023-12-21 20:29:54 +0100

    ion button

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -6,7 +6,10 @@ import { Component } from '@angular/core';
   styleUrls: ['home.page.scss'],
 })
 export class HomePage {
+  text = 'Default Starting Text';
 
+  onChangeText() {
+    this.text = 'Changed!';
+  }
   constructor() {}
-
 }
